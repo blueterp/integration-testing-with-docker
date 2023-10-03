214411282a2b1d75f322758a8f5eca159b98c2e0
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-10-03 04:41:37 +0100

    create simple user model for demonstration

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from typing import Optional
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
