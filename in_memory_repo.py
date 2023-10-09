b92d9e7f756093a7428406307a01b4cf96f5d73e
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-10-09 23:31:50 +0100

    lint repo files

--- in_memory_repo.py
+++ in_memory_repo.py
@@ -1,17 +1,16 @@
 from repo_interface import Repo
 
+
 class InMemoryRepo(Repo):
     def __init__(self, data=None):
-            self.data = dict() if data is None else data
-
+        self.data = dict() if data is None else data
 
     def create_user(self, user_info):
         self.data[user_info["email"]] = user_info
-        return 
+        return
 
     def get_user(self, user_email):
         return self.data[user_email]
-    
+
     def get_users(self):
         return self.data
-    
--- repo_interface.py
+++ repo_interface.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
+
+
 class Repo(ABC):
     @abstractmethod
     def create_user(self, user_info):
         pass
-    
+
     @abstractmethod
     def get_user(self, user_email):
         pass
-    
-    
+
     @abstractmethod
     def get_users(self):
         pass
