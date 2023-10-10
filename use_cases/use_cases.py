d09a26ac16b05b02f36abf7e8f20ed8e15eeeb76
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-10-10 04:24:55 +0100

    fix lint issues with use_cases

--- use_cases/use_cases.py
+++ use_cases/use_cases.py
@@ -1,12 +1,19 @@
+"""
+Use cases (following clean architecture styel) for interacting with domain 
+objects and persistent storage
+"""
+
 from repository.repo_interface import Repo
 from models.user import User
 
 
 def create_user(user: User, repo: Repo):
+    """Create user in database."""
     user_info = user.as_dict()
     repo.create_user(user_info)
 
 
 def get_user(email: str, repo: Repo):
+    """Get user from database."""
     user_info = repo.get_user(email)
     return User(**user_info)
