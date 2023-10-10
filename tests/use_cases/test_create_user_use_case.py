35d37d8ed1912906cc5f803bfe5203b95329ddea
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-10-10 04:13:47 +0100

    update use cases and tests

---
+++ tests/use_cases/test_create_user_use_case.py
@@ -0,0 +1,28 @@
+import pytest
+from repository.in_memory_repo import InMemoryRepo
+from models.user import User
+from use_cases.use_cases import create_user, get_user
+
+
+@pytest.fixture
+def repo():
+    return InMemoryRepo()
+
+
+@pytest.fixture
+def user():
+    return User("blueterp@gmail.com", "Jonathan Howarth")
+
+
+def test_create_non_existing_user(repo, user):
+    create_user(user, repo)
+    assert repo.data.get("blueterp@gmail.com") == user.as_dict()
+
+
+def test_get_existing_user(repo, user):
+    repo.create_user({"email": "blueterp@gmail.com", "fullname": "Jonathan Howarth"})
+    assert get_user("blueterp@gmail.com", repo) == user
+
+
+if __name__ == "__main__":
+    pytest.main()
---
+++ use_cases/use_cases.py
@@ -0,0 +1,12 @@
+from repository.repo_interface import Repo
+from models.user import User
+
+
+def create_user(user: User, repo: Repo):
+    user_info = user.as_dict()
+    repo.create_user(user_info)
+
+
+def get_user(email: str, repo: Repo):
+    user_info = repo.get_user(email)
+    return User(**user_info)
